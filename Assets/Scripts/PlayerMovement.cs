449b459d6d3742f59d9b41fc74c0ce83a2803a8b
Emrecan <emrecan.senel2001@gmail.com>
2025-08-06 21:40:15 +0300

    Collision and tilemap

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,39 +2,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float slideSpeed = 5f;
+    public float moveSpeed = 10f;
+    public LayerMask obstacleLayer;
 
-    Vector2 direction;
-    bool hitWall = false;
+    private Vector2 moveDirection;
+    private bool isMoving = false;
 
-    void Start()
+    private void Update()
     {
-
-    }
-
-    void Update()
-    {
-        MoveToDirection();
-    }
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Çarpýþma");
-    }
-
-    void MoveToDirection()
-    {
-        float movementX;
-        float movementY;
-
-        if (!hitWall)
+        if (!isMoving)
         {
-            movementX = Input.GetAxis("Horizontal");
-            movementY = Input.GetAxis("Vertical");
-            direction = new Vector2(movementX, movementY) * Time.deltaTime;
+            // Input al
+            if (Input.GetKeyDown(KeyCode.W)) TryMove(Vector2.up);
+            else if (Input.GetKeyDown(KeyCode.S)) TryMove(Vector2.down);
+            else if (Input.GetKeyDown(KeyCode.A)) TryMove(Vector2.left);
+            else if (Input.GetKeyDown(KeyCode.D)) TryMove(Vector2.right);
+        }
+    }
+
+    void TryMove(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, obstacleLayer);
+
+        if (hit.collider != null)
+        {
+            Vector2 targetPosition = hit.point - direction * 0.5f; // Biraz boþluk býrak
+            StartCoroutine(MoveToPosition(targetPosition));
+        }
+    }
+
+    System.Collections.IEnumerator MoveToPosition(Vector2 target)
+    {
+        isMoving = true;
+
+        while ((Vector2)transform.position != target)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+            yield return null;
         }
 
-        transform.Translate(direction * slideSpeed);
+        isMoving = false;
     }
 }
